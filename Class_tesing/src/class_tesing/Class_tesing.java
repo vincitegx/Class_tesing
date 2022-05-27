a398173025c0b5a6b40a6e85a9dce0ea04af7f03
David Tega <40434274+vincitegx@users.noreply.github.com>
2022-05-27 04:49:25 +0100

    Add files via upload

---
+++ Class_tesing/src/class_tesing/Class_tesing.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class_tesing;
+
+import javafx.geometry.Point2D;
+import javafx.geometry.Point3D;
+
+/**
+ *
+ * @author user
+ */
+public class Class_tesing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Character  c = 's';
+        System.out.println(c);
+        Point2D p = new Point2D(11.0,13.5);
+        Point3D d = new Point3D(11.0,12.3,13.7);
+        
+        
+    }
+    
+}
